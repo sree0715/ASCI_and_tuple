baec02a6249235f51ab08597de384c495df518e0
sree0715 <125061287+sree0715@users.noreply.github.com>
2023-02-21 22:55:16 +0530

    Add files via upload

---
+++ 6_days.py
@@ -0,0 +1,21 @@
+# print(chr(73))
+# print(ord("f"))
+# print(chr(0))
+# print(chr(37))
+
+# t=2,5
+# print(dir(t))
+
+# b=45,78,"4.5",8.3
+# print(b.index("4.5"))
+# print(b.index(5)) #valueerror:
+
+# print(b.count(8)) #0
+# print(b.count(78))
+
+
+
+# d=("r,t,h,j")
+# print(type(d))
+# print(d.index("r"))
+
